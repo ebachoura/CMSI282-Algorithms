a69da3de548f6462d663ae2879fb4eafc03fc5a9
ebachoura <edlobacro@gmail.com>
2017-02-28 01:10:43 -0800

    putting the tuple class within the SumoSolver class

--- 3. SumoSolver/SumoSolver.java
+++ 3. SumoSolver/SumoSolver.java
@@ -42,7 +42,6 @@ public class SumoSolver {
   }
 
   private void solveHelp(int x, int y) {
-    // System.out.println("visited: " + x + ", " + y);
     if (y == 0) {
       if (isNull(x, y) && x >= prices[0]) { chart[x][y] = new Tuple(prices.length, 0); }
     } else {
@@ -51,7 +50,6 @@ public class SumoSolver {
         if (isNull(x - prices[y], y - 1)) { solveHelp(x - prices[y], y - 1); }
         if (!isNull(x - prices[y], y - 1) && (chart[x - prices[y]][y - 1].price(prices) + chart[x][y].price(prices)) <= x) {
           chart[x][y] = chart[x][y].add(chart[x - prices[y]][y - 1]);
-          // if (x == budget + 1 && y == prices.length) { System.out.println("            EDIT (" + x + ", " + y + ") = " + chart[x][y].toString(prices, gains)); }
         }
       }
       if (isNull(x, y - 1)) { solveHelp(x, y - 1); }
@@ -59,9 +57,6 @@ public class SumoSolver {
         if (!isNull(x, y - 1) && isNull(x, y) || isGreater(chart[x][y-1], chart[x][y])) {
           chart[x][y] = chart[x][y - 1];
         }
-      }
-      if (!isNull(x, y)) {
-        // System.out.println("final (" + x + ", " + y + ") = " + chart[x][y].toString(prices, gains));
       }
     }
   }
@@ -72,5 +67,90 @@ public class SumoSolver {
 
   private boolean isNull(int x,int  y) {
     return chart[x][y] == null;
+  }
+  public class Tuple {
+    private int[] data;
+    public Tuple(int n) {
+      if (n < 0) {
+        throw new IllegalArgumentException();
+      }
+      data = new int[n];
+      for (int i = 0; i < n; i++) {
+        data[i] = 0;
+      }
+    }
+
+    public Tuple(int n, int index) {
+      data = new int[n];
+      for (int i = 0; i < n; i++) {
+        if (i == index) {
+          data[i] = 1;
+        } else {
+          data[i] = 0;
+        }
+      }
+    }
+
+    private void setElement(int i, int j) {
+      data[i] = j;
+    }
+
+    public int getElement(int i) {
+      return data[i];
+    }
+
+    public int length() {
+      return data.length;
+    }
+
+    public Tuple add(Tuple t) {
+      Tuple sum = new Tuple(length());
+      for (int i = 0; i < length(); i++) {
+        sum.setElement(i, getElement(i) + t.getElement(i));
+      }
+
+      return sum;
+    }
+
+    public String toString(int[] p, int[] g) {
+      if (this.data == null) {
+        return "0 items / $0 / 0 pounds";
+      }
+      return (itemsListed(p, g) + (itemCount() == 1? "1 item / " : (itemCount() + " items / ")) + "$" + price(p) + " / " + gains(g) + " pounds");
+    }
+
+    private int itemCount() {
+      int count = 0;
+      for (int i = 0; i < data.length; i++) {
+        count+=data[i];
+      }
+      return count;
+    }
+
+    public int gains(int[] g) {
+      int gains = 0;
+      for (int i = 0; i < g.length; i++) {
+        gains += g[i]*data[i];
+      }
+      return gains;
+    }
+
+    public int price(int[] p) {
+      int cost = 0;
+      for (int i = 0; i < p.length; i++) {
+        cost += (p[i]*data[i]);
+      }
+      return cost;
+    }
+
+    private String itemsListed(int[] p, int[] g) {
+      String s = "";
+      for (int i = 0; i < data.length; i++) {
+        if (data[i] == 1) {
+          s = s + "$" + p[i] + " / " + g[i] + " pounds \n" + "\n";
+        }
+      }
+      return s;
+    }
   }
 }
