75343f3449598d82b42bdd27f5962d3db74e99de
ebachoura <edlobacro@gmail.com>
2017-04-27 10:20:55 -0700

    changed to an iterative method and generalized the program for any 10 values that you input through standard in

--- 6. Pentagon Problem Solver/PentagonSolver.java
+++ 6. Pentagon Problem Solver/PentagonSolver.java
@@ -1,48 +1,62 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 class PentagonSolver {
   public static void main (String[] args) {
-    int[] set = new int[]{0,1,2,3,4,5,6,7,8,9};
-    ArrayList<int[]> solutionSet = new ArrayList<int[]>();
-    traverseThroughPermutations(set, solutionSet);
-    // System.out.println(print(solutionSet));
-  }
-
-  public static void traverseThroughPermutations(int[] currentCombination, ArrayList<int[]> solutionSet) {
-    if (Arrays.equals(currentCombination, new int[]{9,8,7,6,5,4,3,2,1,0})) { return; }
-
-    int lastIndex = currentCombination.length - 1;
-    if (currentCombination[lastIndex] > currentCombination[lastIndex - 1]) {
-      currentCombination = swap(currentCombination, lastIndex, lastIndex - 1);
+    int[] set = new int[]{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    if (args.length == 0) {
+      set = new int[]{0,1,2,3,4,5,6,7,8,9};
+    } else if (args.length != 10) {
+      System.out.println("If you do not input EXACTLY ten intergers, this program can not run it's algorithms, try again.");
+      return;
     } else {
-      int count = lastIndex;
-      while (currentCombination[count] < currentCombination[count - 1]) { count--; } count--;
-      currentCombination = swapAndOrder(currentCombination, count);
+      for (int i = 0; i < args.length; i++) {
+        int currentArg = Integer.parseInt(args[i]);
+        if (IntStream.of(set).anyMatch(x -> x == currentArg)) {
+          System.out.println("You have two or more values of equal value, please try again but with no repeated values.");
+          return;
+        }
+        set[i] = currentArg;
+      }
     }
-    System.out.println(Arrays.toString(currentCombination));
-    // if (satisfies(currentCombination)) { solutionSet.add(currentCombination); }
-    traverseThroughPermutations(currentCombination, solutionSet);
+    insertSort(set, 0);
+    traverseThroughPermutations(set);
   }
 
-  private static int[] swap(int[] currentArray, int x, int y) {
+  public static void traverseThroughPermutations(int[] currentCombination) {
+    int lastIndex = currentCombination.length - 1;
+    int[] reversedSet = reverseSet(currentCombination);
+    if (satisfiesPentagonProblemCondition(currentCombination)) { System.out.println(Arrays.toString(currentCombination)); }
+
+    while (!Arrays.equals(currentCombination, reversedSet)) {
+      if (currentCombination[lastIndex] > currentCombination[lastIndex - 1]) {
+        swap(currentCombination, lastIndex, lastIndex - 1);
+      } else {
+        int count = lastIndex;
+        while (currentCombination[count] < currentCombination[count - 1]) { count--; } count--;
+        swapAndOrder(currentCombination, count);
+      }
+      if (satisfiesPentagonProblemCondition(currentCombination)) { System.out.println(Arrays.toString(currentCombination)); }
+    }
+  }
+
+  private static void swap(int[] currentArray, int x, int y) {
     int tempHold = currentArray[x];
     currentArray[x] = currentArray[y];
     currentArray[y] = tempHold;
-    return currentArray;
   }
 
-  private static int[] swapAndOrder(int[] currentArray, int x) {
+  private static void swapAndOrder(int[] currentArray, int x) {
     int nextSmallestValueIndex = x + 1;
     for (int i = x + 1; i < currentArray.length; i++) {
       if (currentArray[nextSmallestValueIndex] > currentArray[i] && currentArray[x] < currentArray[i]) { nextSmallestValueIndex = i; }
     }
-    currentArray = swap(currentArray, x, nextSmallestValueIndex);
-    currentArray = insertSort(currentArray, x + 1);
-    return currentArray;
+    swap(currentArray, x, nextSmallestValueIndex);
+    insertSort(currentArray, x + 1);
   }
 
-  private static int[] insertSort (int[] listToSort, int left) {
+  private static void insertSort (int[] listToSort, int left) {
     int temp;
     for (int i = left; i < listToSort.length; i++) {
       for(int j = i ; j > left ; j--){
@@ -53,27 +67,24 @@ class PentagonSolver {
         }
       }
     }
-    return listToSort;
   }
 
-  private static boolean satisfies (int[] arrayToTest) {
-    int sum1 = arrayToTest[0] + arrayToTest[1] + arrayToTest[2];
-    int sum2 = arrayToTest[2] + arrayToTest[3] + arrayToTest[4];
-    int sum3 = arrayToTest[4] + arrayToTest[5] + arrayToTest[6];
-    int sum4 = arrayToTest[6] + arrayToTest[7] + arrayToTest[8];
-    int sum5 = arrayToTest[8] + arrayToTest[9] + arrayToTest[0];
-    if (sum1 == sum2 && sum1 == sum3 && sum1 == sum4 && sum1 == sum5) {
-      return true;
-    }
-    return false;
+  private static boolean satisfiesPentagonProblemCondition (int[] arrayToTest) {
+    return arrayToTest[0] + arrayToTest[1] + arrayToTest[2] == arrayToTest[2] + arrayToTest[3] + arrayToTest[4] && arrayToTest[0] + arrayToTest[1] + arrayToTest[2] == arrayToTest[4] + arrayToTest[5] + arrayToTest[6] && arrayToTest[0] + arrayToTest[1] + arrayToTest[2] == arrayToTest[6] + arrayToTest[7] + arrayToTest[8] && arrayToTest[0] + arrayToTest[1] + arrayToTest[2] == arrayToTest[8] + arrayToTest[9] + arrayToTest[0];
   }
 
-  private static String print(ArrayList<int[]> solutionSet) {
-    String print = "";
-    for (int i = 0; i < solutionSet.size(); i++) {
-      print = print + java.util.Arrays.toString(solutionSet.get(i));
-      if (i != solutionSet.size() - 1) { print = print + "\n"; }
+  private static int[] reverseSet (int[] listToSort) {
+    int[] listToReverse = listToSort.clone();
+    int temp;
+    for (int i = 0; i < listToReverse.length; i++) {
+      for(int j = i ; j > 0 ; j--){
+        if(listToReverse[j] > listToReverse[j-1]) {
+          temp = listToReverse[j];
+          listToReverse[j] = listToReverse[j - 1];
+          listToReverse[j - 1] = temp;
+        }
+      }
     }
-    return print;
+    return listToReverse;
   }
 }
